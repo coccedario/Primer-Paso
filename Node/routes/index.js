452e6fcce3047c488977a0f7fae6f31f51f5f51a
dario-cocce <dcocce@bvconline.com.ar>
2023-06-15 22:57:14 -0300

    Node Routes

--- Node/routes/index.js
+++ Node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DArio' });
+  res.render('index', { title: 'Dario´s for Sale' });
 });
 
 module.exports = router;
